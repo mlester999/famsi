3d4dd42408275ec5ac822cd4ad45669e5a2b0a70
Mark Lester Acak <acakmarklester33@gmail.com>
2023-08-02 20:18:33 +0800

    Configured the calendar and added an event

--- resources/js/Components/TableHRManagerDashboard.vue
+++ resources/js/Components/TableHRManagerDashboard.vue
@@ -1,27 +1,74 @@
 <script setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import FullCalendar from "@fullcalendar/vue3";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import PopupCalendarModal from "./PopupCalendarModal.vue";
+import { useForm } from "@inertiajs/vue3";
 
 const props = defineProps({
     events: Array,
 });
 
-const handleDateSelect = () => {
+const form = useForm({
+    title: "",
+    startDate: "",
+    endDate: "",
+});
+
+const optionsModalVisibility = ref(false);
+
+const formattedStartTime = computed(() => {
+    const [datePart, timePart] = form.startDate.value.split("T");
+    const [time, timezoneOffset] = timePart.split(/[+|-]/);
+    const [hours, minutes] = time.split(":");
+    const amPm = hours >= 12 ? "PM" : "AM";
+    const formattedHours = hours % 12 || 12;
+    return `${formattedHours}:${minutes}${amPm}`;
+});
+
+const formattedEndTime = computed(() => {
+    const [datePart, timePart] = form.endDate.value.split("T");
+    const [time, timezoneOffset] = timePart.split(/[+|-]/);
+    const [hours, minutes] = time.split(":");
+    const amPm = hours >= 12 ? "PM" : "AM";
+    const formattedHours = hours % 12 || 12;
+    return `${formattedHours}:${minutes}${amPm}`;
+});
+
+const hideOptionsModal = () => {
+    document.body.classList.remove("overflow-hidden");
+
+    optionsModalVisibility.value = false;
+};
+
+const showOptionsModal = () => {
+    document.body.classList.add("overflow-hidden");
+
+    optionsModalVisibility.value = true;
+};
+
+const handleDateSelect = (arg) => {
+    console.log(arg);
     alert("Handle Data Select was clicked");
 };
 
-const handleEventClick = () => {
-    alert("Handle Event was clicked");
+const handleEventClick = (arg) => {
+    form.title = arg.event.title;
+    form.startDate = arg.event.startStr;
+    form.endDate = arg.event.endStr;
+    showOptionsModal();
+    console.log(arg);
 };
 
-const handleEventDrop = () => {
+const handleEventDrop = (arg) => {
+    console.log(arg);
     alert("Event Dropped was clicked");
 };
 
-const handleEventResize = () => {
+const handleEventResize = (arg) => {
+    console.log(arg);
     alert("Event Resize was clicked");
 };
 
@@ -48,13 +95,21 @@ const calendarOptions = ref({
     weekends: false,
     events: props.events,
 });
-
-const toggleWeekends = () => {
-    calendarOptions.value.weekends = !calendarOptions.value.weekends;
-};
 </script>
 
 <template>
+    <Transition
+        enter-active-class="transition ease-in-out duration-300"
+        leave-active-class="transition ease-in-out duration-300"
+    >
+        <Teleport to="body">
+            <div
+                v-if="optionsModalVisibility"
+                @click="hideOptionsModal"
+                class="bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-30 transition duration-200"
+            ></div>
+        </Teleport>
+    </Transition>
     <div
         class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800"
     >
@@ -77,6 +132,7 @@ const toggleWeekends = () => {
             <div class="overflow-x-auto rounded-lg">
                 <div class="inline-block min-w-full align-middle">
                     <div class="overflow-hidden shadow sm:rounded-lg"></div>
+
                     <FullCalendar :options="calendarOptions" />
                 </div>
             </div>
@@ -92,4 +148,71 @@ const toggleWeekends = () => {
             >Fully Advanced Manpower Solutions, Inc.</a
         >. All rights reserved.
     </p>
+
+    <!-- Deactivation Product Drawer -->
+    <Transition
+        enter-from-class="translate-x-full"
+        enter-active-class="transition-transform translate-x-0"
+        leave-active-class="transition-transform translate-x-0"
+        leave-to-class="translate-x-full"
+    >
+        <div
+            v-if="optionsModalVisibility"
+            id="drawer-delete-product-default"
+            class="fixed top-0 right-0 z-40 w-full h-screen max-w-xs p-4 overflow-y-auto bg-white dark:bg-gray-800"
+            tabindex="-1"
+            aria-labelledby="drawer-label"
+            aria-hidden="true"
+        >
+            <h5
+                id="drawer-label"
+                class="inline-flex items-center text-sm font-semibold text-gray-500 uppercase dark:text-gray-400"
+            >
+                Select Options
+            </h5>
+            <button
+                @click="hideOptionsModal"
+                type="button"
+                aria-controls="drawer-delete-product-default"
+                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+                <svg
+                    aria-hidden="true"
+                    class="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                    ></path>
+                </svg>
+                <span class="sr-only">Close menu</span>
+            </button>
+
+            <h3 class="mb-10 mt-8 text-lg text-gray-500 dark:text-gray-400">
+                <span class="font-bold text-black">Time: </span>
+                <span>{{ formattedStartTime }} - {{ formattedEndTime }}</span>
+            </h3>
+
+            <h3 class="mb-10 mt-8 text-lg text-gray-500 dark:text-gray-400">
+                <span class="font-bold text-black">Title: </span>
+                <span>{{ form.title }}</span>
+            </h3>
+
+            <button
+                class="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm inline-flex items-center px-3 py-2.5 text-center mr-2 dark:focus:ring-blue-900"
+            >
+                Edit
+            </button>
+
+            <button
+                class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-3 py-2.5 text-center mr-2 dark:focus:ring-red-900"
+            >
+                Delete
+            </button>
+        </div>
+    </Transition>
 </template>
